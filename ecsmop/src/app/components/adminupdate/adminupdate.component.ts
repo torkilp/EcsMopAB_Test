fa827b82a6e38faa3b7104f8f53b3ce547220a4a
Torkil Pedersen <torkil.pedersen@gmail.com>
2025-03-13 00:58:32 +0100

    feat: integrate Bootstrap for styling; enhance admin update component with tabs and buttons

--- ecsmop/src/app/components/adminupdate/adminupdate.component.ts
+++ ecsmop/src/app/components/adminupdate/adminupdate.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
+import { SoftwareUpdate } from '../../entities/softwareupdate';
 
 
 
@@ -12,11 +13,22 @@ import { NgFor, NgIf } from '@angular/common';
 export class AdminUpdateComponent implements OnInit {
   title = 'Admin Update';
 
+  softwareupdatelist: Array<SoftwareUpdate> = new Array<SoftwareUpdate>();
+
+  constructor() { }
+
 
   ngOnInit() {
-
+    this.initData();
   }
 
+  initData(): void {
+
+    this.softwareupdatelist.push(Object.assign(new SoftwareUpdate(), { id: "DSP_121", date: "24-01-31", description: "Windows Update", status: "Recommended", info: "Windows Update" }));
+    this.softwareupdatelist.push(Object.assign(new SoftwareUpdate(), { id: "DSP_122", date: "24-01-31", description: "Windows Update", status: "Security", info: "Security Update" }));
+    this.softwareupdatelist.push(Object.assign(new SoftwareUpdate(), { id: "DSP_123T", date: "24-01-31", description: "Windows Update", status: "Recommended", info: "Windows Update" }));
+
+  }
 
 
 
