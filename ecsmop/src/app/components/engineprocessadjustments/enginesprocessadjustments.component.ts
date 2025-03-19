06cbb823dcb6c5bc50f98c0807512b9cb0a69706
Torkil Pedersen <torkil.pedersen@gmail.com>
2025-03-19 07:17:29 +0100

    feat: add engine process adjustments component with tab navigation and styling

---
+++ ecsmop/src/app/components/engineprocessadjustments/enginesprocessadjustments.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
+
+
+
+@Component({
+  selector: 'engine-status',
+  templateUrl: './enginesprocessadjustments.component.html',
+  styleUrl: './enginesprocessadjustments.component.css'
+})
+export class EnginesProcessAdjustmentsComponent implements OnInit {
+  title = 'Engine Status';
+
+
+  ngOnInit() {
+
+  }
+
+
+
+
+
+}
